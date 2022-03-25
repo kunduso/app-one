0104cdaed571887a77fd592b99805268107338f9
Sourav Kundu <sourav12kundu@gmail.com>
2022-03-25 10:16:27 -0500

    debugging installed modules

--- InstallAWSTools.ps1
+++ InstallAWSTools.ps1
@@ -1,28 +1,30 @@
 $ListofModulesInstalled = (Get-InstalledModule).Name
-Write-Host -Message "Checking if AWS.Tools.Installer is installed on this instance."
+Write-Host "Checking if AWS.Tools.Installer is installed on this instance."
+
+Write-Host "List of installed modules: $ListofModulesInstalled"
 if ($ListofModulesInstalled -contains "AWS.Tools.Installer")
 { 
-    Write-Host -Message "AWS.Tools.Installer module exists."
+    Write-Host "AWS.Tools.Installer module exists."
 }else { 
-    Write-Host -Message "AWS.Tools.Installer module does not exist and needs to be installed."
+    Write-Host "AWS.Tools.Installer module does not exist and needs to be installed."
     $ListofPackagesInstalled = (Get-PackageProvider -ListAvailable).Name
-    Write-Host -Message "AWS.Tools.Installer requires nuget package version 2.8.5.201 or above to be installed. Checking if correct version of nuget package is installed."
+    Write-Host "AWS.Tools.Installer requires nuget package version 2.8.5.201 or above to be installed. Checking if correct version of nuget package is installed."
     if ($ListofPackagesInstalled -contains "Nuget")
     {
-        Write-Host -Message "Nuget package exists. Checking version."
+        Write-Host "Nuget package exists. Checking version."
         $CheckNugetVersion=(get-PackageProvider -Name NuGet).Version
         if($CheckNugetVersion -ge "2.8.5.201")
         {
-            Write-Host -Message "Nuget version is $CheckNugetVersion and that is acceptable."
+            Write-Host "Nuget version is $CheckNugetVersion and that is acceptable."
         }else {
-            Write-Host -Message "Nuget version is $CheckNugetVersion and a newer package will be installed."
+            Write-Host "Nuget version is $CheckNugetVersion and a newer package will be installed."
             Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
         }
     } else {
-        Write-Host -Message "Nugest package does not exists and will be installed."
+        Write-Host "Nugest package does not exists and will be installed."
         Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     }
     Install-Module -Name AWS.Tools.Installer -Force
-    Install-Module -Name AWSPowerShell.NetCore -Force
-    Write-Host -Message "AWS.Tools.Installer was installed successfully."
+    Install-Module -Name AWSPowerShell.Common -Force
+    Write-Host "AWS.Tools.Installer was installed successfully."
 }
